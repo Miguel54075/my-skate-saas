3e3c2e8ab71efdd4f54c7270adcbf1bdcff9dfaf
Miguel Oliveira <migmaster200@gmail.com>
2026-08-22 18:37:37 -0300

    fix: adiciona ip 0.0.0.0 no http server listen

--- server/app.js
+++ server/app.js
@@ -107,7 +107,6 @@ app.get('/health', async (req, res) => {
 
 const PORT = process.env.PORT || 3333;
 
-// Adicione o '0.0.0.0' antes da porta
 httpServer.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Servidor Street Burger com WebSockets rodando na porta ${PORT}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
